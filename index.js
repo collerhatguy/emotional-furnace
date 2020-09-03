c62f72973b558061ccf8caa8aa808dc35fd2d55b
collerhatguy <jacobshomali3@gmail.com>
2020-09-03 17:12:04 -0400

    animation

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const burnButton = document.querySelector("button");
 const negativity = document.getElementById("negative");
-console.log(negativity);
 
 burnButton.addEventListener("click", burn);
 
 function burn() {
-  negativity.classList.add("burning");
-  setTimeout((negativity.value = ""), 2500);
-  setTimeout(negativity.classList.remove("burning"), 5000);
+  negativity.className = "burning";
+  setTimeout((negativity.value = ""), 2.5);
 }
